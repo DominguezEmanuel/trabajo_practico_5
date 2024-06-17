477ee09005c4d91e87e028a1a2684670fdf2be9d
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-06-16 21:17:51 -0300

    Se agrega la estructura del proyecto

---
+++ trabajo_practico_5/src/main/java/ar/edu/unju/fi/TrabajoPractico5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPractico5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPractico5Application.class, args);
+	}
+
+}
