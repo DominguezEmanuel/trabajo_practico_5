0d5975142c396e53b70d9b65aab4fdc125193282
fibia <fibia@GWTN156-4>
2024-06-29 19:43:43 -0300

    Interfaces de repositorio listas y app.properties cambiada

--- trabajo_practico_5/src/main/java/ar/edu/unju/fi/repository/CarreraRepository.java
+++ trabajo_practico_5/src/main/java/ar/edu/unju/fi/repository/CarreraRepository.java
@@ -1,5 +1,9 @@
 package ar.edu.unju.fi.repository;
 
-public interface CarreraRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.edu.unju.fi.model.Carrera;
+
+public interface CarreraRepository extends JpaRepository<Carrera, Integer>{
 
 }
