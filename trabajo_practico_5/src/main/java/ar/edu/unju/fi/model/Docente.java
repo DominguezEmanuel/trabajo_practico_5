08a3dd9f9c7bd28fc1b7e02df7134074c41fefd8
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-06-30 21:57:54 -0300

    Se configura la clase Docente para mapearla a la base de datos

--- trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -2,29 +2,46 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Component
 
 
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 
-
+@Entity(name = "DOCENTES")
 public class Docente {
-
+	
+	@Id
+	@Column(name = "Codigo")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	@NonNull
+	@Column(name = "Legajo" , nullable = false)
 	private String legajo;
+	@NonNull
+	@Column(name = "Nombre" , nullable = false)
 	private String nombre;
+	@NonNull
+	@Column(name = "Apellido" , nullable = false)
 	private String apellido;
+	@NonNull
+	@Column(name = "Email" , nullable = false)
 	private String email;
+	@NonNull
+	@Column(name = "Telefono" , nullable = false)
 	private String telefono;
-	
-	
-	
 	
 }
