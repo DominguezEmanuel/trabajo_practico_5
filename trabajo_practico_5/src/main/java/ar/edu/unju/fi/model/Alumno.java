d154070077fe006e404d7f7f951672f85b38d58d
fibia <fibia@GWTN156-4>
2024-07-01 00:21:34 -0300

    Cree los Repositorios que faltaban, añadí los  mapeos de todos los objetos e hice las relaciones de Alumno con materia y carrera

--- trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,14 +1,25 @@
 package ar.edu.unju.fi.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Component
@@ -18,16 +29,41 @@ import lombok.Setter;
 @Getter
 @Setter
 
+@Entity(name = "ALUMNOS")
 public class Alumno {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Dni_alumno")
 	private int dni;
+	
+	@NonNull
+	@Column(name = "Nombre" , nullable = false)
 	private String nombre;
+	@NonNull
+	@Column(name = "Apellido" , nullable = false)
 	private String apellido;
+	@NonNull
+	@Column(name = "Email" , nullable = false)
 	private String email;
+	@NonNull
+	@Column(name = "Telefono" , nullable = false)
 	private String telefono;
+	@NonNull
+	@Column(name = "Fecha_nacimiento" , nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate fechaNacimiento;
+	@NonNull
+	@Column(name = "Domicilio" , nullable = false)
 	private String domicilio;
+	@NonNull
+	@Column(name = "Lu" , nullable = false)
 	private String lu;
+	
+	@ManyToMany(mappedBy = "alumnos") //MAPEO RELACION ALUMNO-MATERIAS
+	private List<Materia> materias = new ArrayList<>();
+	@ManyToOne  //MAPEO RELACION ALMUNO-CARRERAS
+	@JoinColumn(name="Codigo_carrera")
+	private Carrera carrera;
 	
 }
--- trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Materia.java
+++ trabajo_practico_5/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,13 +1,24 @@
 package ar.edu.unju.fi.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Component
@@ -17,14 +28,30 @@ import lombok.Setter;
 @Getter
 @Setter
 
-
+@Entity(name = "MATERIAS")
 public class Materia {
 
+	@Id
+	@Column(name = "Codigo_materia")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigo;
+	@NonNull
+	@Column(name = "Nombre" , nullable = false)
 	private String nombre;
+	@NonNull
+	@Column(name = "Curso" , nullable = false)
 	private String curso;
+	@Column(name = "Cantidad_horas" , nullable = false)
 	private byte cantidadHoras;
+	@NonNull
+	@Column(name = "Modalidad" , nullable = false)
 	private String modalidad;
+	
+	@ManyToMany //mapeo relacion alumnos-materias
+	@JoinTable(name = "materias_alumnos",
+	joinColumns= @JoinColumn(name = "Codigo_materia"),
+	inverseJoinColumns = @JoinColumn(name="Dni_alumno"))
+	private List<Alumno> alumnos = new ArrayList<>();
 	
 	@Autowired
 	private Docente docente;
