e17e606ed0a643c82b5ce1d5b8c92bc8e05228a5
fibia <fibia@GWTN156-4>
2024-07-03 10:10:19 -0300

    html de alumnos-carrera, controlador, importsql

---
+++ trabajo_practico_5/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into carreras(codigo_carrera , nombre , cantidad_anos , estado) values (1 , 'Contador Público' , 5 , 1);
+insert into carreras(codigo_carrera , nombre , cantidad_anos , estado) values (2 , 'Nutrición' , 5 , 1);
+
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (1 , '45753951' , 'Juan' , 'Salas' , 'juan1890@gmail.com' , '388415789' , '2000-12-22' , 'Mariano Moreno' , 1);
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (2, '32456789', 'María', 'González', 'maria.gonzalez@example.com', '388512345', '1998-07-15', 'Avenida Siempre Viva 742', 1);
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (3, '76543210', 'Luis', 'Martínez', 'luis.martinez@example.com', '388678945', '1999-11-30', 'Calle Falsa 123', 2);
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (4, '23456789', 'Ana', 'Pérez', 'ana.perez@example.com', '388123456', '1997-03-22', 'Boulevard de los Sueños 456', 2);
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (5, '12243210', 'Ignacio', 'Martínez', 'ignacio.martinez@example.com', '388643945', '2000-11-30', 'Calle Real 123', 1);
+insert into alumnos(lu , dni_alumno , nombre , apellido , email , telefono , fecha_nacimiento , domicilio , codigo_carrera) values (6, '34256789', 'Daniel', 'Pérez', 'Dani.perez@example.com', '388125856', '1997-08-23', 'Brandsen 805', 2);
